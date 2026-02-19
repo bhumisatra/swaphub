edd5059f7b1ace6c02854ba62978bcc942690929
Bhumi Satra <bhumisatra17@gmail.com>
2026-02-19 17:14:03 +0530

    UI improved

--- src/utils/presence.js
+++ src/utils/presence.js
@@ -1,23 +1,31 @@
 import { getDatabase, ref, onDisconnect, set, onValue } from "firebase/database";
 import { auth } from "../firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 export function setupPresence(community) {
-  const uid = auth.currentUser?.uid;
-  if (!uid || !community) return;
 
   const rtdb = getDatabase();
-  const connectedRef = ref(rtdb, ".info/connected");
-  const userStatusRef = ref(rtdb, `status/${community}/${uid}`);
 
-  onValue(connectedRef, (snap) => {
-    if (snap.val() === true) {
+  onAuthStateChanged(auth, (user) => {
+    if (!user || !community) return;
 
-      onDisconnect(userStatusRef).remove();
+    const uid = user.uid;
 
-      set(userStatusRef, {
-        online: true,
-        lastActive: Date.now()
-      });
-    }
+    const connectedRef = ref(rtdb, ".info/connected");
+    const userStatusRef = ref(rtdb, `status/${community}/${uid}`);
+
+    onValue(connectedRef, (snap) => {
+      if (snap.val() === true) {
+
+        // mark offline when tab closes
+        onDisconnect(userStatusRef).remove();
+
+        // mark online
+        set(userStatusRef, {
+          online: true,
+          lastActive: Date.now()
+        });
+      }
+    });
   });
 }
